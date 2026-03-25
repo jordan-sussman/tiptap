ba4f5508fbc46679b129c2253df26d4c6015472b
Jordan Sussman <52428171+jordan-sussman@users.noreply.github.com>
2026-03-24 22:33:39 -0700

    undo last change

--- packages/extension-link/src/helpers/pasteHandler.ts
+++ packages/extension-link/src/helpers/pasteHandler.ts
@@ -33,7 +33,7 @@ export function pasteHandler(options: PasteHandlerOptions): Plugin {
         })
 
         const link = find(textContent, { defaultProtocol: options.defaultProtocol }).find(
-          item => item.isLink && item.start === 0 && item.end === textContent.length,
+          item => item.isLink && item.value === textContent,
         )
 
         if (!textContent || !link || (shouldAutoLink !== undefined && !shouldAutoLink(link.value))) {
